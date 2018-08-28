6d84cbf127b1525553fcf7a958ef8d3d8bca1f01
Nicholas DiChiaro <ndichiaro@gmail.com>
2018-08-27 21:24:14 -0400

    add bootstrap; add banner with img;

---
+++ src/js/containers/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Banner from './Banner';
+import '../../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Banner />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/js/containers/Banner.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../../css/Banner.css';
+
+class Banner extends Component {
+    render(){
+        return(
+            <div className="jumbotron">
+            </div>
+        );
+    }
+}
+export default Banner;
