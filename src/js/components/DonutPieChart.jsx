aed61b29f84adf8d99a70a06d69584373f78ede9
Nicholas DiChiaro <ndichiaro@gmail.com>
2018-09-12 23:48:55 -0400

    update look of pie chart, enhance mouse actions

--- src/js/components/DonutPieChart.jsx
+++ src/js/components/DonutPieChart.jsx
@@ -10,6 +10,7 @@ class DonutPieChart extends Component{
         }
 
         this.onPieEnter = this.onPieEnter.bind(this);
+        this.onPieChartLeave = this.onPieChartLeave.bind(this);
     }
     
     onPieEnter(data, index){
@@ -18,43 +19,42 @@ class DonutPieChart extends Component{
         });
     }
 
+    onPieChartLeave(){
+        let that = this;
+
+        setTimeout(function () {
+            that.setState({
+                activeIndex: -1,
+            });
+        }, 500);
+    }
+
     renderActiveShape(props){
         const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
             fill, payload, percent, value } = props;
-        const RADIAN = Math.PI / 180;
-        const sin = Math.sin(-RADIAN * midAngle);
-        const cos = Math.cos(-RADIAN * midAngle);
-        const sx = cx + (outerRadius + 10) * cos;
-        const sy = cy + (outerRadius + 10) * sin;
-        const mx = cx + (outerRadius + 50) * cos;
-        const my = cy + (outerRadius + 50) * sin;
-        const ex = mx + (cos >= 0 ? 1 : -1) * 50;
-        const ey = my;
-        const textAnchor = cos >= 0 ? 'start' : 'end';
 
         return (
             <g>
-                <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
+                <text x={cx} y={cy - 10} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
+                <text x={cx} y={cy + 10} dy={8} textAnchor="middle" fill={fill}>{`${Math.round(percent * 100)}%`}</text>
                 <Sector
                     cx={cx}
                     cy={cy}
-                    innerRadius={innerRadius}
-                    outerRadius={outerRadius}
-                    startAngle={startAngle}
-                    endAngle={endAngle}
-                    fill={fill}
-                />
-                <Sector
-                    cx={cx}
-                    cy={cy}
-                    startAngle={startAngle}
-                    endAngle={endAngle}
-                    innerRadius={outerRadius + 10}
+                    innerRadius={innerRadius + 20}
                     outerRadius={outerRadius + 20}
+                    startAngle={startAngle}
+                    endAngle={endAngle}
                     fill={fill}
                 />
-                <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} stroke-width="4" fill="none"/>
-                <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill={fill}>{`${Math.round(percent * 100)}%`}</text>
+                <Sector
+                    cx={cx}
+                    cy={cy}
+                    startAngle={startAngle}
+                    endAngle={endAngle}
+                    innerRadius={innerRadius}
+                    outerRadius={innerRadius + 10}
+                    fill={fill}
+                />
             </g>
         );
     }
@@ -64,16 +64,18 @@ class DonutPieChart extends Component{
                   {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
         const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
         return(
-            <PieChart width={1000} height={800}>
+            <PieChart width={650} height={650} onMouseLeave={this.onPieChartLeave}>
                 <Pie
                     data={data}
                     outerRadius={200}
-                    innerRadius={160}
+                    innerRadius={140}
                     activeIndex={this.state.activeIndex}
                     activeShape={this.renderActiveShape}
                     onMouseEnter={this.onPieEnter}
+                    dataKey="value"
+                    nameKey="name"
                 >
-                    { data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>) }
+                    { data.map((entry, index) => <Cell key={index} fill={COLORS[index % COLORS.length]}/>) }
                 </Pie>
             </PieChart>
         )
