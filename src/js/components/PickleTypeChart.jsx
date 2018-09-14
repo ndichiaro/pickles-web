ee67a98b3360514bd461f6695d8932490bb5332b
Nick DiChiaro <ndichiaro@gmail.com>
2018-09-14 15:02:01 -0400

    add title and description to type chart.

--- src/js/components/PickleTypeChart.jsx
+++ src/js/components/PickleTypeChart.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DonutPieChart from './DonutPieChart';
+import '../../css/PickleTypeChart.css';
 
 class PickleTypeChart extends Component{
     render(){
@@ -9,7 +10,10 @@ class PickleTypeChart extends Component{
         return(
             <div className="row">
                 <div className="col-lg">
-                    One of three columns
+                    <div className="pickle-type-description">
+                        <h2>One to rule them all?</h2>
+                        <p>We all love pickles, but which one separates itself from the rest?</p>
+                    </div>
                 </div>
                 <div className="col-lg">
                     <DonutPieChart data={this.props.data} colors={COLORS} outerRadius={200} innerRadius={140} dataKey="value"
